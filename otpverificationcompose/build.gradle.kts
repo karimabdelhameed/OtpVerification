5c2bc93f334451709ea8afe42a62031be5987a5b
karimabdelhameed <karim.abdelhameed2909@gmail.com>
2025-02-15 00:17:16 +0200

    - publish

--- otpverificationcompose/build.gradle.kts
+++ otpverificationcompose/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    `maven-publish`
+//    signing
 }
 
 android {
@@ -35,6 +37,84 @@ android {
         compose = true
     }
 }
+
+// Maven publishing configuration
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.karimabdelhameed"
+                artifactId = "OtpVerification"
+                version = "1.0.0-alpha03"
+            }
+        }
+    }
+//    publishing {
+//        publications {
+//            register<MavenPublication>("release") {
+//                groupId = "io.github.karimabdelhameed"
+//                artifactId = "otp-verification-compose"
+//                version = "1.0.0-alpha03"
+//                artifact("$buildDir/outputs/aar/otpverificationcompose-release.aar") {
+//                    classifier = "release" // Prevents duplicate artifacts
+//                }
+//
+//                from(components.getByName("release"))
+//
+//                pom {
+//                    name.set("otp-verification-compose")
+//                    description.set("A Jetpack Compose library for OTP verification.")
+//                    url.set("https://github.com/karimabdelhameed/OtpVerification")
+//
+//                    licenses {
+//                        license {
+//                            name.set("The Apache License, Version 2.0")
+//                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+//                        }
+//                    }
+//
+//                    developers {
+//                        developer {
+//                            id.set("karimabdelhameed")
+//                            name.set("Karim")
+//                            email.set("karim.abdelhameed2909@gmail.com")
+//                        }
+//                    }
+//
+//                    scm {
+//                        connection.set("scm:git:https://github.com/karimabdelhameed/OtpVerification.git")
+//                        developerConnection.set("scm:git:https://github.com/karimabdelhameed/OtpVerification.git")
+//                        url.set("https://github.com/karimabdelhameed/OtpVerification")
+//                    }
+//                }
+//            }
+//        }
+//
+//        // Publish to Maven Central
+//        repositories {
+//            maven {
+//                name = "sonatype"
+//                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+////                val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+////                val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+////                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+//
+//                credentials {
+//                    username = findProperty("ossrhUsername") as String? ?: ""
+//                    password = findProperty("ossrhPassword") as String? ?: ""
+//                }
+//            }
+//        }
+//    }
+//
+//    // GPG signing
+//    signing {
+//        useGpgCmd()
+//        sign(publishing.publications)
+//    }
+}
+
 
 dependencies {
 
